17d13e3ec3dcbc8649ebc8421956462225062f5a
storres20 <sanchotorres777@gmail.com>
2022-11-09 19:40:53 -0500

    Add stringLength function and testing file

---
+++ test/stringLength.test.js
@@ -0,0 +1,17 @@
+const stringLength = require('../stringLength')
+
+test('string length', ()=>{
+  expect(stringLength('hola')).toBe(4)
+})
+
+test('throw error when string length is greater than 10', () => {
+  expect(() => {
+    stringLength('hello world');
+  }).toThrow('Error Message');
+});
+
+test('throw error when string length is less than 1', () => {
+  expect(() => {
+    stringLength('');
+  }).toThrow('Error Message');
+});
